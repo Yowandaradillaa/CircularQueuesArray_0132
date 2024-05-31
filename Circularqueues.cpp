c518f3aba5f9cefa3cf1416558786f09c46c4c66
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-31 11:01:52 +0700

    Memasukan while

--- Circularqueues.cpp
+++ Circularqueues.cpp
@@ -80,7 +80,11 @@ class Queues {
             }
             FRONT_position = 0;
 
-            
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
 
     }
