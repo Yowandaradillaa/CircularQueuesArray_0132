e67c66f939f0f5a2137ce653ee712affb81374ed
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-31 10:33:12 +0700

    Memasukan if

--- Circularqueues.cpp
+++ Circularqueues.cpp
@@ -19,5 +19,8 @@ void insert () {
     cin >> num;
     cout << endl;
 
-    
+    if ((FRONT == 0 && REAR == max -1) ||(FRONT == REAR + 1)) {
+        cout << "\nQueue overflow\n";
+        return;
+    }
 }
