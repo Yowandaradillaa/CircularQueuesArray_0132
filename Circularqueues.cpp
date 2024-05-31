aa7ca6a1791e4bbfacbed2b155a4895cf0715483
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-31 10:44:07 +0700

    Memasukan if else

--- Circularqueues.cpp
+++ Circularqueues.cpp
@@ -13,31 +13,48 @@ class Queues {
 
 };
 
-void insert () {
-    int num;
-    cout << "Enter a number : ";
-    cin >> num;
-    cout << endl;
+    void insert () {
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
 
-    if ((FRONT == 0 && REAR == max -1) ||(FRONT == REAR + 1)) {
-        cout << "\nQueue overflow\n";
-        return;
+        if ((FRONT == 0 && REAR == max -1) ||(FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+
+        else {
+            if (REAR == max -1)
+            REAR = 0;
+            else 
+            REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 
-    if (FRONT == -1) {
-        FRONT = 0;
-        REAR = 0;
+    void remove (){
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            if (FRONT == max -1)
+            FRONT = 0;
+            else 
+            FRONT = FRONT + 1;
+        }
     }
 
-    else {
-        if (REAR == max -1)
-        REAR = 0;
-        else 
-        REAR = REAR + 1;
-    }
-    queue_array[REAR] = num;
-}
-
-void remove (){
     
-}
